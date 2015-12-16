ff0bd9e1650ede02886023db6f16be0594a537f9
= <sassela@live.com>
2015-12-16 06:27:19 +0000

    Write Mapper isKeyword test

---
+++ src/test/scala/MapperTest.scala
@@ -0,0 +1,25 @@
+/**
+ * Created by abby on 13/12/15.
+ */
+import org.scalatest._
+import scala.io.Source
+
+class MapperTest extends FlatSpec {
+
+  "isKeyWord" should "return boolean value corresponding to non stop-word" in {
+    assert(Mapper.isKeyWord("a") == false)
+    assert(Mapper.isKeyWord("sensible") == false)
+    assert(Mapper.isKeyWord("panda") == true)
+    assert(Mapper.isKeyWord("ukdtyirsdtliviyfodftktckudytd") == true)
+  }
+
+//  "getKeyWords" should "return all keywords for a given string" in {
+//
+//  }
+//
+//  "map" should "parse list of strings into a map of keywords and line numbers" in {
+//
+//  }
+
+}
+
