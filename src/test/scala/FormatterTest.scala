953e1433da044094605d9c5c05c9af146c9cae20
= <sassela@live.com>
2015-12-19 13:15:03 +0000

    Write Formatter formatLine tests. Tidy up other Formatter tests

--- src/test/scala/FormatterTest.scala
+++ src/test/scala/FormatterTest.scala
@@ -9,28 +9,38 @@ class FormatterTest extends FlatSpec {
   "cleanString" should "lowercase all words and remove all digits and all punctuation except apostrophes" in {
 
     assert(Formatter.cleanString("Hello, panda.") == "hello panda")
-    assert(Formatter.cleanString(pandaText) == "a beautiful cocktail table book full of beautiful photos of china and" +
-      " pandas")
+    assert(Formatter.cleanString(pandaText) ==
+      "a beautiful cocktail table book full of beautiful photos of china and pandas")
     assert(Formatter.cleanString("sËØOm8E947. €π4∫Ÿ~ÌΩdeCIp8HeËØrAble 7tEx∂ƒt!") == "some decipherable text")
   }
 
-  "rightJustify" should "return a list of string(s), right justified, up to 30 characters preceding the keyword(s)" in {
+  "rightJustify" should "return a string, right justified, up to 30 characters preceding the keyword(s)" in {
 
-    assert(Formatter.rightJustify(pandaText, Formatter.getMatched(pandaText,"pandas").next()) == ("%30s" format "beautiful photos of China and "))
+    assert(Formatter.rightJustify(pandaText, Formatter.getMatched(pandaText,"pandas").next()) ==
+      ("%30s" format "beautiful photos of China and "))
     assert(Formatter.rightJustify(pandaText, Formatter.getMatched(pandaText,"beautiful").next()) == ("%30s" format "A "))
-    assert(Formatter.rightJustify(pandaText, Formatter.getMatched(pandaText,"table").next()) == ("%30s" format "A beautiful cocktail "))
+    assert(Formatter.rightJustify(pandaText, Formatter.getMatched(pandaText,"table").next()) ==
+      ("%30s" format "A beautiful cocktail "))
   }
 
-  "leftJustify" should "return a list of string(s), left justified, up to 30 characters following the keyword(s)" in {
+  "leftJustify" should "return a string, left justified, up to 30 characters following the keyword(s)" in {
     assert(Formatter.leftJustify(pandaText, Formatter.getMatched(pandaText,"pandas").next()) == ("%-30s" format "."))
-    assert(Formatter.leftJustify(pandaText, Formatter.getMatched(pandaText,"beautiful").next()) == ("%-30s" format " cocktail table book full of b"))
-    assert(Formatter.leftJustify(pandaText, Formatter.getMatched(pandaText,"china").next()) == ("%-30s" format " and Pandas."))
+    assert(Formatter.leftJustify(pandaText, Formatter.getMatched(pandaText,"beautiful").next()) ==
+      ("%-30s" format " cocktail table book full of b"))
+    assert(Formatter.leftJustify(pandaText, Formatter.getMatched(pandaText,"china").next()) ==
+      ("%-30s" format " and Pandas."))
 
   }
 
-//  "formatString" should "lowercase all words and remove all digits and all punctuation except apostrophes" in {
-//
-//  }
+  "formatLine" should "return a list of formatted string(s)" in {
+    assert(Formatter.formatLine(4, "pandas", pandaText) ==
+      List("004 " + ("%30s" format "beautiful photos of China and ") + "Pandas" + ("%-30s" format ".")))
+    assert(Formatter.formatLine(4, "beautiful", pandaText) ==
+      List("004 " + ("%30s" format "A ") + "beautiful" + ("%-30s" format " cocktail table book full of b"),
+      "004 " + ("%30s" format "l cocktail table book full of ") + "beautiful" + ("%-30s" format  "photos of china and pandas")))
+    assert(Formatter.formatLine(4, "cocktail", pandaText) == List("004 " + ("%30s" format "A beautiful ") +
+      "cocktail" + ("%-30s" format  " table book full of beautiful ")))
+  }
 
 
 }
