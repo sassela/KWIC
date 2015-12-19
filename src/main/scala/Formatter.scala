0efe8fd9c394ea6e9ba2d65a56444db825a9e62e
= <sassela@live.com>
2015-12-19 10:07:38 +0000

    Add (messy) Formatter rightJustify method

--- src/main/scala/Formatter.scala
+++ src/main/scala/Formatter.scala
@@ -1,8 +1,32 @@
 /**
  * Created by abby on 18/12/15.
  */
+
+import scala.collection.mutable.ListBuffer
+import scala.util.matching.Regex.Match
+
 object Formatter {
+  // TODO tidy me.
 
   def cleanString(text: String) : String = "[^a-z\\s']+".r replaceAllIn(text.toLowerCase, "")
 
+  def getMatched(textLine: String, keyword: String) : Iterator[Match] = {
+    ("\\s*" + keyword + "\\s*").r findAllMatchIn cleanString(textLine)
+  }
+
+  def rightJustify(textLine: String, keyword: String) = {
+    val matched = getMatched(textLine, keyword)
+    val sentences = new ListBuffer[String]
+
+    matched.foreach { sentence =>
+      if (sentence.start > 30) {
+        sentences += textLine.substring(sentence.start-30, sentence.start)
+      } else {
+        val precedingText = textLine.substring(0, sentence.start)
+        sentences += (" " * (30 - precedingText.length)) + precedingText
+      }
+    }
+
+    sentences.toList
+  }
 }
